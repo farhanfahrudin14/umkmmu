090acbc0888edb96c938e2e7c27b908dba38f74b
farhanfahrudin14 <fm011404@gmail.com>
2025-05-25 16:37:43 +0700

    fix: generate sitemap and robots

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next-sitemap').IConfig} */
+const fetch = require("node-fetch");
+
+async function getDynamicPaths() {
+  const res = await fetch('https://pengunjung-umkmmu.vercel.app/api/umkms');
+  const data = await res.json();
+
+  return data.map((umkm) => `/umkm/${umkm.id}`);
+}
+
 module.exports = {
   siteUrl: 'https://pengunjung-umkmmu.vercel.app',
   generateRobotsTxt: true,
@@ -6,5 +14,17 @@ module.exports = {
   changefreq: 'weekly',
   priority: 0.7,
   outDir: './public',
-  sourceDir: '.next', // <--- ini wajib kalau pake app router
+  sourceDir: '.next', // wajib kalau pakai app router
+  transform: async (config, path) => {
+    return {
+      loc: path, // path already includes `/umkm/id`
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: new Date().toISOString(),
+    };
+  },
+  additionalPaths: async (config) => {
+    const dynamicPaths = await getDynamicPaths();
+    return dynamicPaths;
+  },
 };
